1a3cddfa57a68f0dbdd13de8a8c29562c284f479
Desktop PC <841192493@qq.ccom>
2020-11-08 20:13:15 +0800

    添加springmvc-02-annotation

---
+++ SpringMVCStudy/springmvc-02-annotation/src/main/java/com/hf/controller/HelloCnotroller.java
@@ -0,0 +1,15 @@
+package com.hf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloCnotroller {
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("msg","hello Springmvc");
+        return "hello";
+    }
+}
---
+++ SpringMVCStudy/springmvc-02-annotation/web/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: JulCat
+  Date: 2020/10/31
+  Time: 14:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+${msg}
+</body>
+</html>
